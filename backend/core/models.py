e40b3870e16273857a244026f4fd86c55f64980a
Katlyn Stoll <katlynstoll@gmail.com>
2026-08-04 17:25:53 -0400

    Adds Plant model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date_observed = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
