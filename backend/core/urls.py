471a85e3cbdccd807535411effdee63d6e7186f4
Katlyn Stoll <katlynstoll@gmail.com>
2026-08-06 16:09:00 -0400

    Adds plant API endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import health_check
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import PlantViewSet, health_check
+
+router = DefaultRouter()
+router.register(r"plants", PlantViewSet)
 
 urlpatterns = [
     path("health/", health_check),
+    path("", include(router.urls)),
 ]
