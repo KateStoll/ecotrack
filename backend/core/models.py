e3cf6ab67e4c8df1301ea95f8cfa796144edcb40
Katlyn Stoll <katlynstoll@gmail.com>
2026-08-10 18:46:11 -0400

    Adds plant observation fields

--- backend/core/models.py
+++ backend/core/models.py
@@ -5,6 +5,8 @@ class Plant(models.Model):
     species = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     date_observed = models.DateField()
+    notes = models.TextField(blank=True)
+    sunlight = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
