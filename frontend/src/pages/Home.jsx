61b8a664c028b502385cb759affad6edb57f0bb7
Katlyn Stoll <katlynstoll@gmail.com>
2026-09-25 14:01:48 -0400

    Add Ecotrack home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <main>
+            <h1>EcoTrack</h1>
+
+            <h2>What would you like to do?</h2>
+
+            <button>Add a Plant</button>
+            <button>View Plants</button>
+        </main>
+    )
+}
+
+export default Home;
