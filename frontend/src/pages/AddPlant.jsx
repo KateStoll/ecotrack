1a75e3f08e53b78eb27d33ba8498f39da4cc6d40
Katlyn Stoll <katlynstoll@gmail.com>
2026-09-25 14:22:09 -0400

    Navigate to plant list after adding plant

--- frontend/src/pages/AddPlant.jsx
+++ frontend/src/pages/AddPlant.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import PlantForm from "../components/PlantForm";
 
 function AddPlant({ onPlantCreated }) {
+
+    const navigate = useNavigate();
+
+    const handlePlantCreated = (newPlant) => {
+        onPlantCreated(newPlant);
+        navigate('/plants');
+    }
+
     return (
         <main>
             <h1>Add A Plant</h1>
 
-            <PlantForm onPlantCreated={onPlantCreated} />            
+            <PlantForm onPlantCreated={handlePlantCreated} />            
         </main>
     )
     
