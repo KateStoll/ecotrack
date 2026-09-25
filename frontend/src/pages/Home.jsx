418037ea818a9097c1f73924b0be1d85ef027bb1
Katlyn Stoll <katlynstoll@gmail.com>
2026-09-25 14:14:44 -0400

    Connect home pages routes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,21 @@
+import { Link } from 'react-router-dom'
+
 function Home() {
     return (
         <main>
+
             <h1>EcoTrack</h1>
 
             <h2>What would you like to do?</h2>
 
-            <button>Add a Plant</button>
+           <Link to="/add-plant">
+            <button> Add a Plant </button>
+           </Link>
+
+           <Link to="/plants">
             <button>View Plants</button>
+           </Link>
+
         </main>
     )
 }
